6e6038262d30ba734459859c01434c258cf77c5f
AbdiMohammad <abdi.mo@northeastern.edu>
2023-07-25 09:59:07 -0400

    feat: add save best

--- trainer.py
+++ trainer.py
@@ -32,7 +32,10 @@ class Trainer:
         self.output_dir = output_dir
         self.es_patience = es_patience
 
+        self.best_train_loss = float('inf')
         self.best_val_loss = float('inf')
+        self.best_val_acc = -1
+
         self.last_val_loss = float('inf')
         self.es_cnt = 0
         self.datetime_now = datetime.now()
@@ -40,8 +43,6 @@ class Trainer:
         if os.path.exists(os.path.join(self.output_dir, "snapshot.pt")):
             print("Loading snapshot")
             self._load_snapshot()
-
-        print(self.local_rank)
         self.model = DDP(self.model, device_ids=[self.local_rank])
 
     def _load_snapshot(self):
@@ -64,7 +65,6 @@ class Trainer:
         print(f"[GPU{self.global_rank}] Epoch {epoch} | Batchsize: {len(next(iter(self.train_loader))[0])} | nBatches: {len(self.train_loader)}")
         self.train_loader.sampler.set_epoch(epoch)
         running_loss = 0.0
-        avg_loss = 0.0
         self.model.train()
         for batch, (X, y) in enumerate(self.train_loader):
             X, y = X.to(self.local_rank), y.to(self.local_rank)
@@ -72,12 +72,11 @@ class Trainer:
             loss = self._run_batch(X, y)
 
             running_loss += loss
-            if batch % 100 == 99:
-                avg_loss, current = running_loss / 100.0, (batch + 1) * len(X)
+            if batch % 10 == 9:
+                current = (batch + 1) * len(X)
                 if self.local_rank == 0:
-                    print(f"avg loss: {avg_loss:>7f} loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-                running_loss = 0.0
-        return avg_loss
+                    print(f"avg loss: {running_loss / (batch + 1):>7f} loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+        return running_loss / (batch + 1)
 
     def _validate(self):
         size = len(self.val_loader.dataset)
@@ -104,11 +103,17 @@ class Trainer:
         torch.save(snapshot, os.path.join(self.output_dir, "snapshot.pt"))
         print(f"Epoch {epoch} | Training snapshot saved at {os.path.join(self.output_dir, 'snapshot.pt')}")
 
-    def _save_best(self):
+    def _save_best(self, epoch):
         filename = f"best_{self.datetime_now.strftime('%Y%m%d_%H%M%S')}.pt"
-        filename_state = f"best_{self.datetime_now.strftime('%Y%m%d_%H%M%S')}_state.pt"
-        torch.save(self.model.module, f"{os.path.join(self.output_dir, filename)}")
-        torch.save(self.model.module.state_dict(), f"{os.path.join(self.output_dir, filename_state)}")
+        best = {
+            "MODEL": self.model.module,
+            "MODEL_STATE": self.model.module.state_dict(),
+            "EPOCHS_RUN": epoch,
+            "VAL_LOSS": self.best_val_loss,
+            "VAL_ACC": self.best_val_acc,
+            "TRAIN_LOSS": self.best_train_loss
+        }
+        torch.save(best, f"{os.path.join(self.output_dir, filename)}")
 
     def train(self, total_epochs: int):
         for epoch in range(self.epochs_run, total_epochs):
@@ -120,15 +125,21 @@ class Trainer:
     def train_and_validate(self, total_epochs: int):
         for epoch in range(self.epochs_run, total_epochs):
             if self.local_rank == 0:
-                print(f"Epoch {epoch+1}\n-------------------------------")
-            self._run_epoch(epoch)
+                print(f"Epoch {epoch}\n-------------------------------")
+            train_loss = self._run_epoch(epoch)
             val_loss, val_acc = self._validate()
             self.lr_scheduler.step(val_loss)
             if self.local_rank == 0 and epoch % self.save_every == 0:
                 self._save_snapshot(epoch)
+
+            if self.local_rank == 0 and train_loss < self.best_train_loss:
+                self.best_train_loss = train_loss
+            if self.local_rank == 0 and val_acc > self.best_val_acc:
+                self.best_val_acc = val_acc
             if self.local_rank == 0 and val_loss < self.best_val_loss:
                 self.best_val_loss = val_loss
-                self._save_best()
+                self._save_best(epoch)
+            
             if self.last_val_loss < val_loss:
                 self.es_cnt += 1
             else:
